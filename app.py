0e24946b5159a2e9cc74a9c88bd0073dfa12ca04
Diwakar121 <diwakargour121@gmail.com>
2021-12-02 17:20:10 +0530

    8

--- app.py
+++ app.py
@@ -53,5 +53,6 @@ def getcaption():
 
  
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    # defining server ip address and port
+    app.run()
